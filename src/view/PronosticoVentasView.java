514807e2e92242d5cd85e7d81c8c6143c11e2c4e
Sebastián <sebastianidrobo@hotmail.com>
2022-08-04 17:43:10 -0500

    pronosticosVentasView agregado

---
+++ src/view/PronosticoVentasView.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Sebastián Idrobo Avirama <idrobo.sebastian@correounivalle.edu.co>
+ */
+public class PronosticoVentasView {
+    /*
+    Inicializamos los atributos de la clase
+    */
+    
+    //Ancho y alto de ventana
+    private int anchoV = 600;
+    private int altoV = 700;
+    
+    //JButtons
+    private JButton btnAgregar;
+    private JButton btnBorrar;
+    private JButton btnModificar;
+    private JButton btnNuevo;
+    
+    //JTables
+    private JTable tblHistorico;
+    private JTable tblPronostico;
+    
+    //JLabels
+    private JLabel lblVenta;
+    private JLabel lblCantidad;
+    
+    //JTextFields
+    private JTextField txtVenta;
+    private JTextField txtCantidad;
+    private JTextField txtPromedioVariacion;
+    
+    //JPanels
+    private JPanel pnlDatosVenta;
+    private JPanel pnlAnios;
+    private JPanel pnlControles;
+    private JPanel pnlVentas;
+    private JPanel pnlPronostico;
+    
+    
+    
+
+    
+}
